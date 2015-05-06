0bee5bdfd986efa33ab372f3d1b33fa786c61d41
Sebastian Scaularo <sebastianscatularo@gmail.com>
2015-05-06 09:16:49 -0300

    Modified configuration to add main class.

--- src/main/java/ar/com/jss/oauth/domain/User.java
+++ src/main/java/ar/com/jss/oauth/domain/User.java
@@ -2,7 +2,6 @@ package ar.com.jss.oauth.domain;
 
 import java.util.Date;
 import java.util.List;
-import javax.script.*;
 
 /**
  * @author Sebastian Scatularo
@@ -14,17 +13,4 @@ public final class User {
     private String password;
     private Date lastLogin;
     private List<Project> projects;
-
-    public static void main(String... args) throws ScriptException {
-        // create a script engine manager
-        ScriptEngineManager factory = new ScriptEngineManager();
-        // create a JavaScript engine
-        ScriptEngine engine = factory.getEngineByName("JavaScript");
-        // evaluate JavaScript code from String
-        engine.eval("()(" +
-                "function() {" +
-                        "print('Hello, World');" +
-                    "}" +
-                ");");
-    }
 }
